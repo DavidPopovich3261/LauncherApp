f01e22ff147934d60d0d1843e40470b2ef9b973b
DavidPopovich3261 <8534397@gmail.com>
2026-03-16 16:08:14 +0200

    client pages

---
+++ client/components/Details.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route,Link } from "react-router";
+
+
+import React from 'react'
+
+function Details() {
+  return (
+    <div>Details</div>
+  )
+}
+
+export default Details
---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from '../pages/Home';
+import AddLauncher from '../pages/AddLauncher';
+import LauncherDetails from '../pages/LauncherDetails';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/AddLauncher' element={<AddLauncher />} />
+          <Route path='/LauncherDetails/:id' element={<LauncherDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
